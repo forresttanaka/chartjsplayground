e24f3b926c39c99cb79fa57c8363061d8c94bdb9
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-08-11 10:39:03 -0700

    Add self-generated legend

--- js/main.js
+++ js/main.js
@@ -30,6 +30,22 @@ var myChart = new Chart(ctx, {
         responsive: false,
         legend: {
             display: false
+        },
+        legendCallback: function(chart) {
+            console.log('LEGEND: %o', chart);
+            var text = [];
+            text.push('<ul>');
+            for (var i = 0; i < chart.data.datasets[0].data.length; i++) {
+                text.push('<li>');
+                text.push('<a href="#' + chart.data.datasets[0].data[i] + '">');
+                text.push('<span style="background-color:' + chart.data.datasets[0].backgroundColor[i] + '"></span>');
+                if (chart.data.labels[i]) {
+                    text.push(chart.data.labels[i]);
+                }
+                text.push('</a></li>');
+            }
+            text.push('</ul>');
+            return text.join('');
         }
     }
 });
